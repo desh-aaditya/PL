6ef899fc352245371e5e8c560fe82831bcbd0895
desh-aaditya <156404441+desh-aaditya@users.noreply.github.com>
2024-03-19 14:53:38 +0530

    Add files via upload

---
+++ complex_struct.c
@@ -0,0 +1,17 @@
+//create structure as complex
+#include<stdio.h>
+struct complex{
+	int real;
+	int img;
+}c1,c2,c3;
+int main()
+{
+	printf("Enter Real part of first number:");
+	scanf("%d",&c1.real);
+	printf("Enter imaginary part of first number:");
+	scanf("%d",&c1.img);
+	printf("Real part of second number:");
+	scanf("%d",&c2.real);
+	printf("Enter imaginary part of second number:");
+	scanf("%d",&c2.img);
+}
